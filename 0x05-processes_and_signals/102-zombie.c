92eb045a28a72bbeb31306bef1f66a62a081d77d
tommydebisi <tommydebisi@gmail.com>
2022-07-23 03:16:11 +0100

    :construction: task11 added (zombie process)

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * infinite_while - function that sleeps
+ *
+ * Return: 0
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - main program
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	pid_t i, child_pid;
+
+	for (i = 0; i < 5; i++)
+	{
+		child_pid = fork();
+		if (child_pid == -1)
+			break;
+		else if (child_pid == 0)
+			exit(0);
+		else
+			printf("Zombie process created, PID: %d\n", child_pid);
+	}
+	infinite_while();
+}
