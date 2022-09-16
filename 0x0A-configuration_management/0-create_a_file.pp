5c9a3d399be74ee8a0ba932836b3ec4712027e26
tommydebisi <tommydebisi@gmail.com>
2022-09-16 23:21:43 +0100

    :seedling: ensure fixed

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # This script creates a file in `tmp`
 
+# ensure set to file makes sure it's a normal file
 file {'/tmp/school':
-  ensure  => link,
-  target  => '/tmp/school'
+  ensure  => file,
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
