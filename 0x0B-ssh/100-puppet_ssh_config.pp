32d109e2a28cdc52d711239f3d2f60c517a56edf
tommydebisi <tommydebisi@gmail.com>
2022-09-18 23:18:37 +0100

    :seedling: return added

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,7 @@
 # This script makes changes to our config files
 
 exec {'echo':
-  command => '/usr/bin/echo "\tIdentityFile ~/.ssh/school\n\tPasswordAuthentication no" >> /etc/ssh/ssh_config'
+  path    => '/usr/bin:/bin'
+  command => 'echo "\tIdentityFile ~/.ssh/school\n\tPasswordAuthentication no" >> /etc/ssh/ssh_config'
+  return  =>  [0, 1]
 }
