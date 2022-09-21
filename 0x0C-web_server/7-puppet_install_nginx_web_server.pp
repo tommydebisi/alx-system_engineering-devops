02d1f99260521f49900922265aa13b49fa362224
tommydebisi <tommydebisi@gmail.com>
2022-09-21 01:57:24 +0100

    :construction: advanced tsk added

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,6 @@
+# Installs a Nginx server
+
+exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; sudo echo "Hello World!" > /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/tommydebisi permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+}
