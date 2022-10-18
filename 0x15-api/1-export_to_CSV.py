bfe274e60acce45fbd904efde9523abc42ad875a
tommydebisi <tommydebisi@gmail.com>
2022-10-18 03:20:37 +0100

    :art: improved structure

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -11,31 +11,26 @@ if __name__ == "__main__":
     try:
         if len(argv) < 2 or type(eval(argv[1])) is not int:
             exit(1)
+
+        count = 1
+        u_id = argv[1]
+
+        todo_url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+            u_id
+        )
+        user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(
+            u_id
+        )
+        # get user's name
+        user = requests.get(user_url)
+        user_name = user.json().get('name')
+
+        # get tasks done by user
+        todo = requests.get(todo_url)
+        with open('{}.csv'.format(u_id), 'x', newline='') as f:
+            writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+            for attr in todo.json():
+                writer.writerow([u_id, user_name, attr.get('completed'),
+                                attr.get('title')])
     except Exception:
         exit(1)
-
-    count = 1
-    u_id = argv[1]
-
-    todo_url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
-        u_id
-    )
-    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(
-        u_id
-    )
-    # get user's name
-    user = requests.get(user_url)
-    user_name = user.json().get('name')
-
-    # get tasks done by user
-    todo = requests.get(todo_url)
-    with open('{}.csv'.format(u_id), 'w', encoding="utf-8") as f:
-        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
-        new_list = list()
-        for attr in todo.json():
-            new_list.append(u_id)
-            new_list.append(user_name)
-            new_list.append(attr.get('completed'))
-            new_list.append(attr.get('title'))
-            writer.writerow(new_list)
-            new_list.clear()
