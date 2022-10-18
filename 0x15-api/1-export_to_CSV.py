1fc40699f687873161543697cefcfa72d307c09e
tommydebisi <tommydebisi@gmail.com>
2022-10-18 03:12:37 +0100

    :seedling: used user id

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -15,11 +15,13 @@ if __name__ == "__main__":
         exit(1)
 
     count = 1
+    u_id = argv[1]
+
     todo_url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
-        argv[1]
+        u_id
     )
     user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(
-        argv[1]
+        u_id
     )
     # get user's name
     user = requests.get(user_url)
@@ -27,11 +29,11 @@ if __name__ == "__main__":
 
     # get tasks done by user
     todo = requests.get(todo_url)
-    with open('USER_ID.csv', 'w', encoding="utf-8") as f:
+    with open('{}.csv'.format(u_id), 'w', encoding="utf-8") as f:
         writer = csv.writer(f, quoting=csv.QUOTE_ALL)
         new_list = list()
         for attr in todo.json():
-            new_list.append(argv[1])
+            new_list.append(u_id)
             new_list.append(user_name)
             new_list.append(attr.get('completed'))
             new_list.append(attr.get('title'))
