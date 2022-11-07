4034a6400370499068cf7704660874d920cefca9
tommydebisi <tommydebisi@gmail.com>
2022-11-07 06:04:31 +0100

    :seedling: use of recurse function for subreddit

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,35 +1,34 @@
 #!/usr/bin/python3
 """
-    This script queries the Reddit API and returns a list
-    containing the titles of all hot articles for a given subreddit
+    This script queries the Reddit API and returns a list containing
+    the titles of all hot articles for a given subreddit
 """
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], after=None):
     """
-    Gets all the hot topics recursively and returns them
+    returns a list containing the titles of all hot articles
+    for a given subreddit
     """
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {
         "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit \
                     /537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
     }
-    index = 0
-    res = requests.get(url, headers=headers)
+
+    params = {'after': after}
+    res = requests.get(url, headers=headers, params=params,
+                       allow_redirects=False)
     if res.status_code == 200:
-        hot_children = res.json().get('data').get('children')
-        helper(hot_children, hot_list, index)
-        return hot_list
+        for index in range(len(res.json().get('data').get('children'))):
+            hot_list.append(res.json().get('data').get(
+                'children')[index].get('data').get('title'))
 
-
-def helper(hot_kids, hot_list, index):
-    """
-        helper function for recursive function
-    """
-    try:
-        hot_list.append(hot_kids[index].get('data').get('title'))
-    except IndexError:
-        return
-
-    helper(hot_kids, hot_list, index + 1)
+        if res.json().get('data').get('after') is not None:
+            return recurse(subreddit, hot_list, res.json()
+                           .get('data').get('after'))
+        else:
+            return hot_list
+    else:
+        return None
